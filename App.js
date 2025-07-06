6b4e090681eeaf7f62c5fa0d90434de29b392999
payalkogde <payalkogde123@gmail.com>
2025-07-06 15:49:59 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // or "react-dom" if React < 18
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App; // ✅ This is what was missing
